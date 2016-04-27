b1498f034affc07394be582bb15684eaebb6a58e
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-27 23:41:14 +0000

    working on list

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 //initial page
  var contactlist_container;
  var top_div;
- var wifi_div;
- var time_div;
- var battery_div;
  var group_div;
  var title_div;
  var addContact_div;
  var search_input;
  var list_div;
- 
+ var current_contact;
+ var new_contact;
+ var new_element;
+
+
+//Groups link setup
+// var a = document.createElement('a');
+// a.setAttribute('href',where link goes);
+// a.innerHTML = 'Groups';
+
+
+var contact_list = ['tiffany', 'khoa', 'veronica', 'nichole', 'alonzo']; 
  
 var create_contact = function(element_type,element_id){
     
@@ -36,15 +44,6 @@ list_div.setAttribute('id','list');
 
  
  //creating content inside top_div
-wifi_div = document.createElement('div');
-wifi_div.setAttribute('id','wifi_div');
-  
-time_div = document.createElement('div');
-time_div.setAttribute('id','time_div');
- 
-battery_div  = document.createElement('div');
-battery_div.setAttribute('id','battery_div');
- 
 group_div  = document.createElement('div');
 group_div.setAttribute('id','group_div');
 
@@ -53,8 +52,7 @@ title_div.setAttribute('id','title_div');
 
 addContact_div = document.createElement('div');
 addContact_div.setAttribute('id','addContact_div');
- 
- // var contact = ['tiffany', 'khoa', 'veronica', 'nichole', 'alonzo'];
+  
 
 
 
@@ -94,13 +92,20 @@ document.addEventListener('DOMContentLoaded',function(event){
    contactlist_container.appendChild(search_input); 
    contactlist_container.appendChild(list_div);
    
-   top_div.appendChild(wifi_div); 
-   top_div.appendChild(time_div);
-   top_div.appendChild(battery_div); 
+    
    top_div.appendChild(group_div);
    top_div.appendChild(title_div); 
    top_div.appendChild(addContact_div);
-    
+   
+   
+   
+  
+   //loop to create contact
+    for(var i=0; i<contact_list.length; i++){
+        create_contact('p','contact_'+i);
+        current_contact = document.getElementById('contact_list_'+i);
+        current_contact.textContent = contact_list[i];
+    };
     
      //document.body.appendChild(input_element);
     //loop to create contact
