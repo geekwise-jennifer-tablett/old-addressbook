ebf2adcf6a672f4d6966ec351f008399b672346e
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-27 23:58:27 +0000

    layout fixed

--- js/script.js
+++ js/script.js
@@ -1,49 +1,23 @@
-//initial page
- var contactlist_container;
- var top_div;
- var group_div;
- var title_div;
- var addContact_div;
- var search_input;
- var list_div;
- var current_contact;
- var new_contact;
- var new_element;
+var top_div;
+var group_div;
+var title_div;
+var addContact_div;
+var search_input;
+var contact;
+var contactlist_container;
+var current_;
+var new_contact;
+var new_element;
+//var input_element;
+
+var contact = ['tiffany', 'khoa', 'veronica', 'nichole', 'alonzo'];
 
 
-//Groups link setup
-// var a = document.createElement('a');
-// a.setAttribute('href',where link goes);
-// a.innerHTML = 'Groups';
-
-
-var contact_list = ['tiffany', 'khoa', 'veronica', 'nichole', 'alonzo']; 
- 
-var create_contact = function(element_type,element_id){
-    
-    var element = document.createElement(element_type);
-    element.setAttribute('id',element_id);
-    contactlist_container.appendChild(element);
-    
-}; 
- 
- 
- //creating initial page
-contactlist_container = document.createElement('div');
-contactlist_container.setAttribute('id','contactlist');
 
 top_div = document.createElement('div');
 top_div.setAttribute('id','top_div');
 
-search_input = document.createElement('input');
-search_input.setAttribute('id','search_display');
-search_input.setAttribute('placeholder','search');
-
-list_div = document.createElement('div');
-list_div.setAttribute('id','list');
-
- 
- //creating content inside top_div
+//creating content inside top_div
 group_div  = document.createElement('div');
 group_div.setAttribute('id','group_div');
 
@@ -52,6 +26,134 @@ title_div.setAttribute('id','title_div');
 
 addContact_div = document.createElement('div');
 addContact_div.setAttribute('id','addContact_div');
+
+search_input = document.createElement('input');
+search_input.setAttribute('id','search_display');
+search_input.setAttribute('placeholder','search');
+
+
+
+//container to hold contact
+contactlist_container = document.createElement('div');
+contactlist_container.setAttribute('id','contactlist');
+
+var create_contact = function(element_type,element_id){
+    var element = document.createElement(element_type);
+    element.setAttribute('id',element_id);
+    contactlist_container.appendChild(element);
+};
+
+
+//create input so users can add contacts
+// input_element = document.createElement('input');
+// input_element.setAttribute('id','contact_display');
+// input_element.setAttribute('placeholder','new contact');
+
+
+//hit enter to add a new contact
+// input_element.addEventListener('keydown',function(event){
+//     if(event.keyCode === 13){
+//         new_contact = this.value;
+//         new_element = document.createElement('p');
+//         contact.push(new_contact)
+//         console.log(new_contact);
+//         new_element.textContent = new_contact;
+//         contactlist_container.appendChild(new_element);
+//          contactlist_container.children[0].parentElement.insertBefore(new_contact, contactlist_container.children[0]);
+//         this.value = null;
+            
+            
+//         };
+    
+//     });
+
+
+
+//when webpage loads
+document.addEventListener('DOMContentLoaded',function(event){
+    
+    //append to body
+    contactlist_container.appendChild(top_div);
+    document.body.appendChild(contactlist_container);
+    
+    top_div.appendChild(group_div);
+    top_div.appendChild(title_div); 
+    top_div.appendChild(addContact_div);
+    
+    contactlist_container.appendChild(search_input);
+    
+    //loop to create contact
+    for(var i=0; i<contact.length; i++){
+        create_contact('p','contact_'+i);
+        current_contact = document.getElementById('contact_'+i);
+        current_contact.textContent = contact[i];
+        
+        
+        
+    };
+    
+    
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Groups link setup
+// var a = document.createElement('a');
+// a.setAttribute('href',where link goes);
+// a.innerHTML = 'Groups';
+
+
+
+ 
+ 
+
+
+
+
+
+
+ 
+ 
   
 
 
@@ -84,28 +186,26 @@ addContact_div.setAttribute('id','addContact_div');
 
 
 //when webpage loads
-document.addEventListener('DOMContentLoaded',function(event){
+//document.addEventListener('DOMContentLoaded',function(event){
     
     //append to body
-   document.body.appendChild(contactlist_container);
-   contactlist_container.appendChild(top_div);
-   contactlist_container.appendChild(search_input); 
-   contactlist_container.appendChild(list_div);
+//   document.body.appendChild(contactlist_container);
+
+//    
+//   contactlist_container.appendChild(list_div);
    
     
-   top_div.appendChild(group_div);
-   top_div.appendChild(title_div); 
-   top_div.appendChild(addContact_div);
+
    
    
    
   
    //loop to create contact
-    for(var i=0; i<contact_list.length; i++){
-        create_contact('p','contact_'+i);
-        current_contact = document.getElementById('contact_list_'+i);
-        current_contact.textContent = contact_list[i];
-    };
+    // for(var i=0; i<contact_list.length; i++){
+    //     create_contact('p','contact_'+i);
+    //     current_contact = document.getElementById('contact_list_'+i);
+    //     current_contact.textContent = contact_list[i];
+    // };
     
      //document.body.appendChild(input_element);
     //loop to create contact
@@ -121,5 +221,5 @@ document.addEventListener('DOMContentLoaded',function(event){
     
     
     
-});
+//});
 
