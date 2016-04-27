4e0bb276ef4b3abdfa7205aa4d0e580e2ef7c1a4
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-27 21:20:32 +0000

    added inner div

--- js/script.js
+++ js/script.js
@@ -16,8 +16,9 @@ var create_contact = function(element_type,element_id){
     var element = document.createElement(element_type);
     element.setAttribute('id',element_id);
     contactlist_container.appendChild(element);
-   
+    
 }; 
+ 
  
  //creating initial page
 contactlist_container = document.createElement('div');
@@ -30,20 +31,30 @@ search_input = document.createElement('input');
 search_input.setAttribute('id','search_display');
 search_input.setAttribute('placeholder','search');
 
-
+list_div = document.createElement('div');
+list_div.setAttribute('id','list');
 
  
+ //creating content inside top_div
+wifi_div = document.createElement('div');
+wifi_div.setAttribute('id','wifi_div');
+  
+time_div = document.createElement('div');
+time_div.setAttribute('id','time_div');
  
+battery_div  = document.createElement('div');
+battery_div.setAttribute('id','battery_div');
  
-// var input_element;
-// var contact;
+group_div  = document.createElement('div');
+group_div.setAttribute('id','group_div');
 
-// var current_contact;
-// var new_contact;
-// var new_element;
+title_div = document.createElement('div');
+title_div.setAttribute('id','title_div');
 
-
-// var contact = ['tiffany', 'khoa', 'veronica', 'nichole', 'alonzo'];
+addContact_div = document.createElement('div');
+addContact_div.setAttribute('id','addContact_div');
+ 
+ // var contact = ['tiffany', 'khoa', 'veronica', 'nichole', 'alonzo'];
 
 
 
@@ -81,8 +92,14 @@ document.addEventListener('DOMContentLoaded',function(event){
    document.body.appendChild(contactlist_container);
    contactlist_container.appendChild(top_div);
    contactlist_container.appendChild(search_input); 
-    
-    
+   contactlist_container.appendChild(list_div);
+   
+   top_div.appendChild(wifi_div); 
+   top_div.appendChild(time_div);
+   top_div.appendChild(battery_div); 
+   top_div.appendChild(group_div);
+   top_div.appendChild(title_div); 
+   top_div.appendChild(addContact_div);
     
     
      //document.body.appendChild(input_element);
